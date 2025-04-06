29b12eeacd91e172099b73c59957106cacdafb14
SergioTomas2004 <sergio.vargas.contact@gmail.com>
2025-04-06 01:09:20 -0600

    Finalized NExt

---
+++ Front-End/Next.js/JS Mastery/promptopia/models/user.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    minlength: 3, // instead of using regex
+    maxlength: 30,
+  },
+
+  image: {
+    type: String,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
